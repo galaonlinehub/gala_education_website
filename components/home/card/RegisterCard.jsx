6c4fbd9ce377d31bf6520a9e61d92090745604a4
rico <rico@gmail.com>
2024-10-17 06:24:18 +0300

    progress on the landing page

--- components/home/card/RegisterCard.jsx
+++ components/home/card/RegisterCard.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 
 
-function Card1({image,title,desc}) {
+function RegisterCard({image,title,desc}) {
   return (
-    <div className='flex-col flex w-[70vw] sm:w-[20vw] shadow-md shadow-black'>
+    <div className='flex-col flex w-[70vw] sm:w-[20vw] shadow-xl shadow-black'>
   {/* Wrap the image in a div */}
   {/* <div className='flex-col flex w-[20vw] h-[12vh]'> */}
   {/* Wrap the image in a div */}
@@ -21,7 +21,7 @@ function Card1({image,title,desc}) {
   
   
   
-  <div className='h-[28vh] bg-[#001840] flex flex-col px-2 py-4'>
+  <div className='h-[20vh] bg-[#001840] flex flex-col px-2 py-4'>
     <span className='text-white font-black'>{title}</span>
     <span className='text-white text-sm'>{desc}</span>
   </div>
@@ -31,4 +31,4 @@ function Card1({image,title,desc}) {
   )
 }
 
-export default Card1
+export default RegisterCard
