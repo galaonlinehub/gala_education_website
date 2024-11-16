81d8b3f6fedceff5404f879c50d9ec7b8fd6111c
Denis mgaya <dnspascal094@gmail.com>
2024-11-16 08:15:35 +0300

    Password change with real time verification

--- app/(auth)/forgot-password/page.jsx
+++ app/(auth)/forgot-password/page.jsx
@@ -1,164 +1,12 @@
-// "use client";
-// import React, { useState } from "react";
-// import { useRouter } from "next/navigation";
-// import { useForm } from "react-hook-form";
-// import { message, Input, Button, Card, Space, Typography } from "antd";
-// import {
-//   MailOutlined,
-//   KeyOutlined,
-//   ArrowLeftOutlined,
-// } from "@ant-design/icons";
-// import { api } from "@/src/config/settings";
-
-// const { Title, Text } = Typography;
-
-// const ForgotPassword = () => {
-//   const router = useRouter();
-// //   const [loading, setLoading] = useState(false);
-//   const [otpSent, setOtpSent] = useState(false);
-//   const [email, setEmail] = useState("");
-
-//   const { register, handleSubmit, formState:{errors, isSubmitting} } = useForm();
-
-//   const onSubmit = async (data) => {
-//     message.success("OTP sent to your email");
-
-//     try {
-//       setLoading(true);
-//       message.destroy();
-//       const response = await api.post("/forgot-password", {
-//         email: data.email,
-//       });
-//       if (response.data.success) {
-//         setEmail(data.email);
-//         setOtpSent(true);
-//         message.success("OTP sent to your email");
-//       } else {
-//         message.error(response.data.message);
-//       }
-//     } catch {
-//       message.error("Failed to send OTP");
-//     } finally {
-//     //   setLoading(false);
-//     }
-//   };
-
-//   const handleOtpSubmit = async (data) => {
-//     try {
-//       setLoading(true);
-//       message.destroy();
-//       const response = await api.post("/verify-otp", { email, otp: data.otp });
-//       if (response.data.success) {
-//         message.success("OTP verified. Reset password now.");
-//         router.push("/reset-password");
-//       } else {
-//         message.error(response.data.message);
-//       }
-//     } catch {
-//       message.error("Failed to verify OTP");
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   return (
-//     <div className="flex items-center justify-center h-screen bg-gray-100">
-//       <Card
-//         className="!w-full !max-w-xl !bg-white !p-8 !rounded-lg !shadow-sm"
-//         title={
-//           <div className="!flex !justify-start !items-center gap-3">
-//             {/* <ArrowLeftOutlined
-//               onClick={() => router.push("/login")}
-//               className="cursor-pointer !self-center"
-//             /> */}
-//             <Title className="!self-center" level={3}>
-//               Forgot Password
-//             </Title>
-//           </div>
-//         }
-//       >
-//         {!otpSent ? (
-//           <form
-//             className="flex flex-col gap-2"
-//             onSubmit={handleSubmit(onSubmit)}    
-//                   >
-//             <div className="mb-4">
-//               <label
-//                 htmlFor="email"
-//                 className="block text-gray-700 font-bold mb-2"
-//               >
-//                 <MailOutlined className="!mr-2" /> Email
-//               </label>
-//               <Input
-//                 id="email"
-//                 type="email"
-//                 placeholder="Enter your email"
-//                 className="!h-input-height"
-//                 {...register("email", {
-//                   required: true,
-//                   pattern: /^\S+@\S+$/i,
-//                 })}
-//               />
-//               {errors.email?.message && (
-//                 <Text type="danger" className="text-sm mt-2">
-//                   {errors.email?.message}
-//                 </Text>
-//               )}
-//             </div>
-//             <Button
-//               type="primary" 
-//               htmlType="submit" 
-//               loading={isSubmitting}
-//               className="!w-full !bg-[#030DFE] !text-white !font-bold !py-5 !px-4 !rounded"
-//             >
-//               Send OTP
-//             </Button>
-//           </form>
-//         ) : (  ""
-//         //   <form onSubmit={handleSubmit(handleOtpSubmit)}>
-//         //     <div className="mb-4">
-//         //       <label
-//         //         htmlFor="otp"
-//         //         className="block text-gray-700 font-bold mb-2"
-//         //       >
-//         //         <KeyOutlined className="mr-2" /> OTP
-//         //       </label>
-//         //       <Input
-//         //         id="otp"
-//         //         type="text"
-//         //         placeholder="Enter the OTP"
-//         //         {...register("otp", { required: true, pattern: /^\d{6}$/ })}
-//         //       />
-//         //       {formState.errors.otp?.message && (
-//         //         <Text type="danger" className="text-sm mt-2">
-//         //           {formState.errors.otp?.message}
-//         //         </Text>
-//         //       )}
-//         //     </div>
-//         //     <Button
-//         //       type="primary" htmlType="submit"
-//         //       loading={loading}
-//         //       className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-//         //     >
-//         //       Verify OTP
-//         //     </Button>
-//         //   </form>
-//         )}
-//       </Card>
-//     </div>
-//   );
-// };
-
-// export default ForgotPassword;
-
-
 "use client";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-import { useForm, Controller } from "react-hook-form"; 
+import { useForm, Controller } from "react-hook-form";
 import { message, Input, Button, Card, Typography } from "antd";
 import { MailOutlined } from "@ant-design/icons";
 import { api } from "@/src/config/settings";
+import EmailVerification from "@/src/components/ui/auth/signup/EmailVerification";
+import { useEmailVerificationModalOpen } from "@/src/store/auth/signup";
 
 const { Title, Text } = Typography;
 
@@ -166,11 +14,12 @@ const ForgotPassword = () => {
   const router = useRouter();
   const [otpSent, setOtpSent] = useState(false);
   const [email, setEmail] = useState("");
+  const { setOpenEmailVerificationModal } = useEmailVerificationModalOpen();
 
   const {
-    control, 
+    control,
     handleSubmit,
-    formState: { errors, isSubmitting }
+    formState: { errors, isSubmitting },
   } = useForm();
 
   const onSubmit = async (data) => {
@@ -186,8 +35,9 @@ const ForgotPassword = () => {
       } else {
         message.error(response.data.message);
       }
-    } catch {
-      message.error("Failed to send OTP");
+    } catch (e) {
+      message.error(`Failed to send OTP, ${e.message}`);
+      router.push('/forgot-password/password-change')
     }
   };
 
@@ -247,11 +97,12 @@ const ForgotPassword = () => {
               loading={isSubmitting}
               className="!w-full !bg-[#030DFE] !text-white !font-bold !py-5 !px-4 !rounded"
             >
-              Send OTP
+              {isSubmitting ? "Sending OTP" : "Send OTP"}
             </Button>
           </form>
         ) : null}
       </Card>
+
     </div>
   );
 };
---
+++ app/(auth)/forgot-password/password-change/page.jsx
@@ -0,0 +1,242 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useForm, Controller } from "react-hook-form";
+import { Input, Button, Card, Typography, message } from "antd";
+import {
+  LockOutlined,
+  EyeOutlined,
+  EyeInvisibleOutlined,
+} from "@ant-design/icons";
+import { api } from "@/src/config/settings";
+import { HiCheckCircle } from "react-icons/hi2";
+import { IoIosCloseCircle } from "react-icons/io";
+
+const { Title, Text } = Typography;
+
+const ChangePassword = ({ email }) => {
+  const router = useRouter();
+  const [passwordChecks, setPasswordChecks] = useState({
+    hasUpperCase: false,
+    hasLowerCase: false,
+    hasNumber: false,
+    hasSpecialChar: false,
+    hasMinLength: false,
+  });
+
+  const {
+    control,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    defaultValues: {
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const password = watch("password");
+
+  useEffect(() => {
+    if (password) {
+      setPasswordChecks({
+        hasUpperCase: /[A-Z]/.test(password),
+        hasLowerCase: /[a-z]/.test(password),
+        hasNumber: /[0-9]/.test(password),
+        hasSpecialChar: /[!@#$%^&*(),.?":{}|<>]/.test(password),
+        hasMinLength: password.length >= 8,
+      });
+    } else {
+      setPasswordChecks({
+        hasUpperCase: false,
+        hasLowerCase: false,
+        hasNumber: false,
+        hasSpecialChar: false,
+        hasMinLength: false,
+      });
+    }
+  }, [password]);
+
+  const allChecksPassed = Object.values(passwordChecks).every(Boolean);
+
+  const PasswordRequirement = ({ satisfied, label }) => (
+    <div className="flex items-center gap-2 text-sm">
+      {satisfied ? (
+        <HiCheckCircle className="text-green-500" />
+      ) : (
+        <IoIosCloseCircle className="text-red-500" />
+      )}
+      <span className={satisfied ? "text-green-500" : "text-red-500"}>
+        {label}
+      </span>
+    </div>
+  );
+
+  //   const validatePassword = (value) => {
+  //     if (!value) return "Password is required";
+  //     if (!allChecksPassed) {
+  //       return "Password must meet all requirements";
+  //     }
+  //     return true;
+  //   };
+
+  const validatePassword = (value) => {
+    if (!value) return "Password is required";
+    if (!allChecksPassed) {
+      return false;
+      //   const missingRequirements = [];
+      //   if (!passwordChecks.hasUpperCase) missingRequirements.push("uppercase letter");
+      //   if (!passwordChecks.hasLowerCase) missingRequirements.push("lowercase letter");
+      //   if (!passwordChecks.hasNumber) missingRequirements.push("number");
+      //   if (!passwordChecks.hasSpecialChar) missingRequirements.push("special character");
+      //   if (!passwordChecks.hasMinLength) missingRequirements.push("minimum length of 8 characters");
+
+      //   return missingRequirements.length > 0
+      //     ? `Missing requirements: ${missingRequirements.join(", ")}`
+      //     : true;
+    }
+    return true;
+  };
+
+  const onSubmit = async (data) => {
+    try {
+      const formData = new FormData();
+      formData.append("email", email);
+      formData.append("newPassword", data.password);
+
+      const response = await api.post("/reset-password", formData);
+
+      if (response.data.success) {
+        message.success("Password changed successfully");
+        router.push("/signin");
+      } else {
+        message.error(response.data.message);
+      }
+    } catch (error) {
+      message.error(`Failed to change password: ${error.message}`);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <Card
+        className="!w-full !max-w-xl !bg-white !p-8 !rounded-lg !shadow-sm"
+        title={
+          <div className="flex justify-start items-center gap-3">
+            <Title className="self-center" level={3}>
+              Change Password
+            </Title>
+          </div>
+        }
+      >
+        <form className="flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              <LockOutlined className="mr-2" /> New Password
+            </label>
+            <Controller
+              name="password"
+              control={control}
+              rules={{
+                validate: validatePassword,
+              }}
+              shouldUnregister={false}
+              render={({ field }) => (
+                <Input.Password
+                  {...field}
+                  id="password"
+                  placeholder="Enter new password"
+                  className="!h-input-height"
+                  iconRender={(visible) =>
+                    visible ? <EyeOutlined /> : <EyeInvisibleOutlined />
+                  }
+                />
+              )}
+            />
+            {errors.password && (
+              <Text type="danger" className="!text-sm !mt-2">
+                {errors.password.message}
+              </Text>
+            )}
+
+            <div className="mt-4 space-y-2 bg-gray-50 p-4 rounded-md">
+              <Text strong className="block mb-2">
+                Password Requirements:
+              </Text>
+              <PasswordRequirement
+                satisfied={passwordChecks.hasMinLength}
+                label="At least 8 characters long"
+              />
+              <PasswordRequirement
+                satisfied={passwordChecks.hasUpperCase}
+                label="Contains at least one uppercase letter"
+              />
+              <PasswordRequirement
+                satisfied={passwordChecks.hasLowerCase}
+                label="Contains at least one lowercase letter"
+              />
+              <PasswordRequirement
+                satisfied={passwordChecks.hasNumber}
+                label="Contains at least one number"
+              />
+              <PasswordRequirement
+                satisfied={passwordChecks.hasSpecialChar}
+                label="Contains at least one special character"
+              />
+            </div>
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="confirmPassword"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              <LockOutlined className="mr-2" /> Confirm Password
+            </label>
+            <Controller
+              name="confirmPassword"
+              control={control}
+              rules={{
+                required: "Confirm password is required",
+                validate: (value) =>
+                  value === password || "Passwords do not match",
+              }}
+              render={({ field }) => (
+                <Input.Password
+                  {...field}
+                  id="confirmPassword"
+                  placeholder="Confirm new password"
+                  className="!h-input-height"
+                  iconRender={(visible) =>
+                    visible ? <EyeOutlined /> : <EyeInvisibleOutlined />
+                  }
+                />
+              )}
+            />
+            {errors.confirmPassword && (
+              <Text type="danger" className="!text-sm !mt-2">
+                {errors.confirmPassword.message}
+              </Text>
+            )}
+          </div>
+
+          <Button
+            type="primary"
+            htmlType="submit"
+            loading={isSubmitting}
+            disabled={!allChecksPassed}
+            className="!w-full !bg-[#030DFE] !text-white !font-bold !py-5 !px-4 !rounded disabled:opacity-50"
+          >
+            {isSubmitting ? "Changing Password..." : "Change Password"}
+          </Button>
+        </form>
+      </Card>
+    </div>
+  );
+};
+
+export default ChangePassword;
