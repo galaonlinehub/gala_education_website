7d0bed2e06da21f92b98f5cc6dd2ff755764144e
salim1007 <107793128+salim1007@users.noreply.github.com>
2024-11-29 23:57:45 +0300

    Revert "Added functionality to add topics together with subtopics"

--- app/providers.js
+++ app/providers.js
@@ -1,7 +1,6 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { useState } from 'react'
 
 export default function Providers({ children }) {
@@ -17,7 +16,6 @@ export default function Providers({ children }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
