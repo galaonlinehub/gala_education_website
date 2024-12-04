9aaae11ec7f1456463aa946e0a659e16bbaee30f
rico <rico@gmail.com>
2024-12-04 22:41:24 +0300

    added logo

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,25 +1,29 @@
-import localFont from "next/font/local";
 import "../globals.css";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
-import Footer from "@/src/components/layout/footer";
-import Link from "next/link";
-import { Popconfirm } from "antd";
-import ChooseAccont from "@/src/components/ui/auth/signup/ChooseAccount";
 import AuthMain from "@/src/components/layout/MainAuth";
 
 export const metadata = {
-  title: "Galahub education",
-  description: "Empowering minds shaping future",
+    title: "Galahub Education",
+    description: "Empowering minds, shaping future",
+    icons: {
+        icon: [
+            { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+            { url: '/favicon.svg', type: 'image/svg+xml' },
+        ],
+        shortcut: [{ url: '/favicon.ico' }],
+        apple: [{ url: '/apple-touch-icon.png', sizes: '180x180' }],
+    },
+    manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className="">
+    return (
+        <html lang="en">
+        <body>
         <AntdRegistry>
-          <AuthMain>{children}</AuthMain>
+            <AuthMain>{children}</AuthMain>
         </AntdRegistry>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
