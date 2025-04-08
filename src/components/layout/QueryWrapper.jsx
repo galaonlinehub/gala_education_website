2edf23fb45ca7802545c1ab14e9a341a58c8c085
Denis mgaya <dnspascal094@gmail.com>
2025-04-08 10:10:04 +0300

    fonts

--- src/components/layout/QueryWrapper.jsx
+++ src/components/layout/QueryWrapper.jsx
@@ -1,25 +1,13 @@
 "use client";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import ClientWrapper from "./ClientWrapper";
-import { ConfigProvider } from 'antd';
-
-
-const config = {
-  theme: {
-    token: {
-      fontFamily: "Quicksand, sans-serif",
-    },
-  },
-};
 
 const queryClient = new QueryClient();
 
 export default function QueryWrapper({ children }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <ConfigProvider {...config}>
-        <ClientWrapper>{children}</ClientWrapper>
-      </ConfigProvider>
+      <ClientWrapper>{children}</ClientWrapper>
     </QueryClientProvider>
   );
 }
