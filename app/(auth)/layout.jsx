ea89f475d4230378bcbfe389ac4c9a87e08d8bf3
salim <salimmchomvu7@gmail.com>
2025-05-11 12:57:29 +0300

    made changes to homepage

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -5,7 +5,7 @@ export default function AuthLayout({ children }) {
   return (
     <div>
       <Navbar />
-      <main className="flex-1 w-screen h-screen overflow-y-scroll max-h-screen pt-16 md:pt-24">
+      <main className="w-screen h-screen overflow-y-scroll max-h-screen pt-16 md:pt-24">
         {children}
       </main>
     </div>
