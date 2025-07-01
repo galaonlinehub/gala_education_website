fcd218249654fb43725b141bd1aae0000d971c09
salim <salimmchomvu7@gmail.com>
2025-07-02 00:02:51 +0300

    changes

--- src/components/teacher/InstructorCompleteProfile.jsx
+++ src/components/teacher/InstructorCompleteProfile.jsx
@@ -71,7 +71,7 @@ const InstructorCompleteProfile = () => {
           </div>
         </div>
       }
-      open={!user?.completed_profile && user?.has_active_subscription}
+      open={user?.completed_profile && user?.has_active_subscription}
       closable={false}
       maskClosable={false}
       footer={null}
@@ -370,7 +370,14 @@ const Save = ({
             </Select>
           </Form.Item>
 
-          <Form.Item name="bio">
+          <Form.Item name="bio" rules={[
+            {
+              required: true,
+              message: (
+                <span className="text-xs ">Bio is required</span>
+              ),
+            },
+          ]}>
             <TextArea
               className="!text-xs !rounded-lg !p-1 !pl-2"
               placeholder="Type your bio. This will be visible to students"
